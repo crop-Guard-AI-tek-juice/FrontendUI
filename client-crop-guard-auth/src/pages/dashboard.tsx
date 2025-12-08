a685f2a320612a7a8064f7d56fd6ce794cf9310c
ayebaishmo <ishmo255super@gmail.com>
2025-12-08 15:36:50 +0300

    Fix and renamed Dashboard to dashboard

--- client-crop-guard-auth/src/pages/dashboard.tsx
+++ client-crop-guard-auth/src/pages/dashboard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Dashboard.css";
+import "./dashboard.css";
 import { auth } from "../firebase";
 import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
